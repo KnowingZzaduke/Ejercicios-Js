4f825af0857819b8b1f5e798b3a0ef51c44ac564
KnowingZzaduke <josex.developer@gmail.com>
2022-11-23 10:42:09 -0500

    Commit

---
+++ ejercicio_3/app.js
@@ -0,0 +1,9 @@
+function numero(n){
+    if(n % 2 === 0){
+        console.log(n, 'es un número par')
+    }else{
+        console.log(n, 'es un número impar')
+    }
+}
+
+let resultado = numero(10)
