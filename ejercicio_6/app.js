a7f38171ef644145ca19529b89e49f8f5087a2c6
KnowingZzaduke <josex.developer@gmail.com>
2022-11-27 21:09:10 -0500

    Commit

--- ejercicio_6/app.js
+++ ejercicio_6/app.js
@@ -1,47 +1,54 @@
-function persona(nombre, edad, genero){
-    this.nombre = nombre;
-    this.edad = edad;
-    this.genero = genero;
-}
-
-//Persona
-persona.prototype.obtDetalles = function(){
-    return (`El nombre de la persona es ${this.nombre}, y tiene ${this.edad} años, su genereo es ${this.genero}`);
-}
-
-const datosPersona = new persona('José', 18, 'Hombre');
-console.log(datosPersona.obtDetalles());
-
-//Estudiante
-function estudiante(curso, grado){
-    this.curso = curso;
-    this.edad = edad;
-}
-
-estudiante.prototype.registrar = function(){
-    return (``)
-}
-
-
-const union1 = {...persona, ...estudiante};
-console.log(union1)
-
-
-function profesor(asignatura, nivel){
-    this.asignatura = asignatura;
-    this.nivel = nivel;
-}
-
-const union2 = {...estudiante, ...profesor};
-console.log(union2);
-
-
-
-
-// persona.prototype.detallesPersona = function(){
-//     return (`El nombre de la persona es ${this.nombre}, tiene ${this.edad} años y su genero es ${this.genero}`)
+// function persona(nombre, edad, genero){
+//     this.nombre = nombre;
+//     this.edad = edad;
+//     this.genero = genero;
 // }
 
+// persona.prototype.obtDetalles = function(){
+//     return (`Su nombre es ${this.nombre}, tiene ${this.edad}, su genero es ${this.genero}`);
+// };
 
-// const datos = new persona('José', 18, 'Hombre');
-// console.log(datos.detallesPersona());
+// function obtenerDatosPersona(){
+//     const datosPersona = new persona('José', 18, 'Hombre');
+//     return (datosPersona.obtDetalles());
+// }
+
+// const resultadoPersona = obtenerDatosPersona()
+// console.log(resultadoPersona)
+
+
+// function estudiante(curso, grupo){
+//     this.curso = curso;
+//     this.grupo = grupo;
+// }
+
+// estudiante.prototype.registrar = function(){
+//     return (`pertenece al curso ${this.curso} y al grupo ${this.grupo}`)
+// }
+
+// function obtenerEstudiante(){
+//     const datosEstudiante = new estudiante('Inglés', 'C1');
+//     return (datosEstudiante.registrar());
+// }
+
+// const resultadoEstudiante = obtenerEstudiante();
+// console.log(resultadoEstudiante);
+
+// Object.setPrototypeOf(persona, estudiante);
+
+// console.log(resultadoPersona.nombre)
+
+const jose = {
+    developer : 'Frontend',
+    años : 18,
+}
+
+const backend = {
+    developer : 'Backend',
+    años: 20,
+    cargo: 'Fullstack'
+}
+
+Object.setPrototypeOf(jose, backend);
+
+console.log(jose.cargo);
